c5299fe5f9fbce3eaa7d410b3442f63d9d7fb2e9
Carlos Gomes <me@cgomesu.com>
2021-01-21 13:54:01 -0300

    tvhlink integration blog post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,60 +2,66 @@ var store = [{
         "title": "Securing access to backends with HAproxy's stick-tables: A guide for pfSense users",
         "excerpt":"Changelog June 23rd, 2020: Moved this guide from Blogger to my website and converted to markdown. Fixed typos I found. June 21st, 2020: This guide stills works with pfsense version 2.4.5-RELEASE-p1. May 1st, 2020: This guide still works with pfsense version 2.4.5-RELEASE and the haproxy packaged version 0.60_4. top Introduction...","categories": ["blog"],
         "tags": ["haproxy","homelab","homeserver","http","pfsense"],
-        "url": "http://localhost:4000/blog/Securing-backend-haproxy-pfsense/",
+        "url": "/blog/Securing-backend-haproxy-pfsense/",
         "teaser": null
       },{
         "title": "The probability of Warp World",
         "excerpt":"This is a repost from my original reddit post. I’ve made a few changes to it to improve readability as well. Introduction If you’re unfamiliar with Warp World, you are in for a treat. It’s a 5RRR sorcery from Ravnica that reads as follows: Each player shuffles all permanents he...","categories": ["blog"],
         "tags": ["mtg","math","probability","model"],
-        "url": "http://localhost:4000/blog/Probability-warp-world/",
+        "url": "/blog/Probability-warp-world/",
         "teaser": null
       },{
         "title": "The math of a bruised souleater",
         "excerpt":"This is a repost of my original reddit post. I’ve made a few changes to the original version to improve readability. This is a post about an interesting interaction that came up yesterday, when I was making changes to an EDH deck. I thought someone else might find this analysis...","categories": ["blog"],
         "tags": ["mtg","math"],
-        "url": "http://localhost:4000/blog/Math-of-bruised-souleater/",
+        "url": "/blog/Math-of-bruised-souleater/",
         "teaser": null
       },{
         "title": "Using the Odroid C2 IR receiver with LibreElec OS",
         "excerpt":"In case you didn’t know, the Odroid C2 comes with an onboard infrared (IR) receiver. Until a few days ago, I thought that such a receiver was only compatible with their own IR remote controller but it turns out you can use it with any IR controller. We can do...","categories": ["blog"],
         "tags": ["odroid","sbc"],
-        "url": "http://localhost:4000/blog/IR-with-odroidc2/",
+        "url": "/blog/IR-with-odroidc2/",
         "teaser": null
       },{
         "title": "Youtube live-streams as IPTV channels for TVHeadend",
         "excerpt":"Changelog September 15th, 2020: There’s a new release of the Youtube4TVH program that does not require the use of API keys. The section about how to create an API key has been updated accordingly. June 23rd, 2020: Moved content from Blogger to my website and converted to markdown. Fixed typos...","categories": ["blog"],
         "tags": ["github","iptv","kodi","streamlink","tvheadend","youtube"],
-        "url": "http://localhost:4000/blog/Youtube-as-IPTV-with-TVH/",
+        "url": "/blog/Youtube-as-IPTV-with-TVH/",
         "teaser": null
       },{
         "title": "The ASRock J3355b-itx pfSense box",
         "excerpt":"Introduction pfSense CE is a free, open-source, and very popular firewall/router that runs on FreeBSD and is developed by Netgate. Even though Netgate sells official pfSense appliances, it’s possible to build your own, custom-made pfSense box for (way) less money (at the expense of way less support from Netgate). In...","categories": ["blog"],
         "tags": ["homelab","homeserver","firewall","network"],
-        "url": "http://localhost:4000/blog/Pfsense-white-box/",
+        "url": "/blog/Pfsense-white-box/",
         "teaser": null
       },{
         "title": "NanoPi M4 mini-NAS",
         "excerpt":"This article is about my mini network-attached storage (NAS) project based on FriendlyARM’s NanoPi M4 and its SATA hat. If you’re looking for a cheap, low-profile, low-power NAS solution for your home–or if you just like single-board computers (SBC)–then this article is for you. Here’s a preview of how my...","categories": ["blog"],
         "tags": ["sbc","arm","homelab","homeserver","storage"],
-        "url": "http://localhost:4000/blog/Nanopi-m4-mini-nas/",
+        "url": "/blog/Nanopi-m4-mini-nas/",
         "teaser": null
       },{
         "title": "Mesh networking: A guide to using free and open-source software with common hardware",
         "excerpt":"Changelog Jan 9th, 2021, Update #2: Added instructions on how to automatically upgrade all installed packages with a single command. This information is in Updating and installing packages. Jan 9th, 2021, Update #1: Added a new section about hardware-specific configurations that are sometimes required for enabling the mesh point mode...","categories": ["blog"],
         "tags": ["mesh","adhoc","ieee","wifi","wireless","radio","network","router","openwrt","batman"],
-        "url": "http://localhost:4000/blog/Mesh-networking-openwrt-batman/",
+        "url": "/blog/Mesh-networking-openwrt-batman/",
         "teaser": null
       },{
         "title": "Repurposing external HDD enclosures into button boxes for the Raspberry Pi",
         "excerpt":"Changelog Dec 18th, 2020: Publication of the original guide top Introduction Hard disk drives (HDDs) are often sold in an external enclosure with easy-to-use interfaces as a detachable, semi-mobile data storage solution. Every so often, however, their price goes below the market price for an equivalent internal HDD and when...","categories": ["blog"],
         "tags": ["DIY","raspberrypi","rpi","hdd","enclosure","button","box"],
-        "url": "http://localhost:4000/blog/Rpi-button-box-ehdd-enclosure/",
+        "url": "/blog/Rpi-button-box-ehdd-enclosure/",
         "teaser": null
       },{
         "title": "Tasmota webcam server for the ESP32-cam",
         "excerpt":"Changelog Jan 16th, 2021: Publication of the original article top Introduction The ESP32 is a cheap and low-power microcontroller developed by Espressif. In addition to its low-cost, the ESP32 is known for its tiny and robust design, the versatility of its applications, and for having onboard Wi-Fi and Bluetooth. It...","categories": ["blog"],
         "tags": ["iot","esp32","tasmota","mqtt","cam","webcam","surveillance","wifi","wireless","network"],
-        "url": "http://localhost:4000/blog/Esp32cam-tasmota-webcam-server/",
+        "url": "/blog/Esp32cam-tasmota-webcam-server/",
+        "teaser": null
+      },{
+        "title": "TVHlink: Livestreams as IPTV channels with TVHeadend and Streamlink",
+        "excerpt":"Changelog Jan 18th, 2021: Publication of the original article top Introduction In my previous post titled Youtube live as IPTV channels for TVHeadend, I mentioned a method for capturing a Youtube livestream and feeding it into a TVHeadend (TVH) server via a Python utility called Streamlink. In this tutorial, I...","categories": ["blog"],
+        "tags": ["github","iptv","kodi","streamlink","tvheadend","youtube","streaming","live"],
+        "url": "/blog/Tvhlink/",
         "teaser": null
       }]
